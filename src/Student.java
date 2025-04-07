f053eec09f64e2f817604a8e29afe6767d0a3dd1
porto <no>
2025-04-07 13:44:30 -0300

    updates

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,18 @@
 public class Student {
+   
+        private String name;
+    
+        public Student(String name) {
+            this.name = name;
+        }
+    
+        public String getName() {
+            return name;
+        }
+    
+        public String toString() {
+            return name;
+        }
+
     
 }
